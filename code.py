5fa1c14f0ae71e6355b4b408ecb9afb2b79c67fa
Javier Garcia-Bernardo <garcia@uva.nl>
2020-10-01 23:25:15 +0200

    repeated add

--- code.py
+++ code.py
@@ -2,66 +2,80 @@
 
 from neo4j import GraphDatabase, basic_auth
 
+driver = GraphDatabase.driver(
+    "bolt://18.209.225.51:33099", 
+    auth=basic_auth("neo4j", "varactors-dives-twirls"))
+session = driver.session()
+
 # driver = GraphDatabase.driver(
-#     "bolt://52.86.81.2:33046", 
-#     auth=basic_auth("neo4j", "totals-costs-stencils"))
+#     "bolt://localhost:7687", 
+#     auth=basic_auth("neo4j", "Dotted-Dragging-Moustache9"))
 # session = driver.session()
 
 
-driver = GraphDatabase.driver(
-    "bolt://localhost:7687", 
-    auth=basic_auth("neo4j", "Dotted-Dragging-Moustache9"))
-session = driver.session()
-
-
-# cypher_query = '''
-# MATCH (n)
-# RETURN id(n) AS id
-# LIMIT 10
-# '''
-
+for q in ["DROP INDEX ON :Company(companyId)",
+			"DROP INDEX ON :GUO(guoId)",
+			"DROP INDEX ON :Director(directorId)",
+			"DROP INDEX ON :Address(addId)"]:
+	session.run(q)
 
 session.run("MATCH (n) DETACH DELETE n")
 
 
-cypher_query1 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/n_dirs.csv' AS row
 CREATE (d:Director {directorId: row.dir_id, directorName: row.dir_name, RTO:row.rto_level})
 """
 
-session.run(cypher_query1)
+session.run(cypher_query)
 
 print("Query 1 completed")
 
-cypher_query2 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/n_comps.csv' AS row
 CREATE (c:Company {companyId: row.bvd_id, companyName: row.company_name, nace: row.nace, legal:row.legal})
 """
 
-session.run(cypher_query2)
+session.run(cypher_query)
 
 print("Query 2 completed")
 
-cypher_query3 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/n_adds.csv' AS row
-CREATE (a:Address {addId: row.company_adddress, addName:row.add_full, addRTOscore:row.add_off})
+CREATE (a:Address {addId: row.company_adddress, addName:row.add_full, perc_RTO:toInteger(row.add_off)})
 """
 
-session.run(cypher_query3)
+session.run(cypher_query)
 
 print("Query 3 completed")
 
+cypher_query = """
+USING PERIODIC COMMIT 10000
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/n_guos.csv' AS row
+CREATE (g:GUO {guoId: row.guo})
+"""
 
-for q in ["CREATE INDEX ON :Company(companyId)","CREATE INDEX ON :Director(directorId)","CREATE INDEX ON :Address(addId)"]:
+session.run(cypher_query)
+
+print("Query 4 completed")
+
+
+
+
+for q in ["CREATE CONSTRAINT compID ON (n:Company) ASSERT n.companyId IS UNIQUE",
+		"CREATE CONSTRAINT guoID ON (n:GUO) ASSERT n.guoId IS UNIQUE",
+		"CREATE CONSTRAINT dirID ON (n:Director) ASSERT n.directorId IS UNIQUE",
+		"CREATE CONSTRAINT addID ON (n:Address) ASSERT n.addId IS UNIQUE"]:
+
 	session.run(q)
 
 print("Index completed")
 
-cypher_query4 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/r_dirs.csv' AS row
 MATCH (c:Company {companyId: row.bvd_id})
 WITH c,row
@@ -69,12 +83,12 @@ MATCH (d:Director {directorId: row.dir_id})
 CREATE (d)-[rd:DIRECTOR_OF]->(c)
 """
 
-session.run(cypher_query4)
+session.run(cypher_query)
 
-print("Query 4 completed")
+print("Query 5 completed")
 
-cypher_query5 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/r_adds.csv' AS row
 MATCH (c:Company {companyId: row.bvd_id})
 with c,row
@@ -82,22 +96,22 @@ MATCH (a:Address {addId: row.company_adddress})
 CREATE (c)-[ra:REGISTERED_AT]->(a)
 """
 
-session.run(cypher_query5)
+session.run(cypher_query)
 
-print("Query 5 completed")
+print("Query 6 completed")
 
-cypher_query6 = """
-USING PERIODIC COMMIT 1000
+cypher_query = """
+USING PERIODIC COMMIT 10000
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/jgarciab/neo4j_test/main/r_guos.csv' AS row
 MATCH (c:Company {companyId: row.bvd_id})
 with c,row
-MERGE (g:GUO {guoId: row.guo})
+MATCH (g:GUO {guoId: row.guo})
 CREATE (c)-[rg:OWNED_BY]->(g)
 """
 
-session.run(cypher_query6)
+session.run(cypher_query)
 
-print("Query 6 completed")
+print("Query 7 completed")
 
 # cypher_query = """
 # USING PERIODIC COMMIT 1000
@@ -120,6 +134,21 @@ print("Query 6 completed")
 for q in ["CREATE INDEX ON :Company(companyName)","CREATE INDEX ON :Director(directorName)","CREATE INDEX ON :Address(add_full)"]:
 	session.run(q)
 
+
+for q in ["REMOVE CONSTRAINT compID ON (n:Company) ASSERT n.companyId IS UNIQUE",
+		"REMOVE CONSTRAINT guoID ON (n:GUO) ASSERT n.guoId IS UNIQUE",
+		"REMOVE CONSTRAINT dirID ON (n:Director) ASSERT n.directorId IS UNIQUE",
+		"REMOVE CONSTRAINT addID ON (n:Address) ASSERT n.addId IS UNIQUE"]:
+
+	session.run(q)
+
+for q in ["CREATE INDEX ON :Company(companyId)",
+			"CREATE INDEX ON :GUO(guoId)",
+			"CREATE INDEX ON :Director(directorId)",
+			"CREATE INDEX ON :Address(addId)"]:
+	session.run(q)
+
 print("QIndex completed")
 
+#C009936572
 
